898c6a21188a6357da0655bc8c7069520161a354
Tapasweni Pathak <tapaswenipathak@gmail.com>
2015-08-16 11:12:48 +0530

    xpaths modified as per the website, autopep8

--- ScrapScrapy/ScrapScrapy/spiders/SSSpider.py
+++ ScrapScrapy/ScrapScrapy/spiders/SSSpider.py
@@ -2,17 +2,18 @@ from scrapy.selector import Selector
 from scrapy.spider import BaseSpider
 from ScrapScrapy.items import ScrapscrapyItem
 
-class ScrapyscrapSpider (BaseSpider) :
-  name = "ss"
-  allowed_domains = ["scrapy.org"]
-  start_urls = ['http://scrapy.org/']
 
-  def parse(self, response) :
-    sel = Selector (response)
-    item = ScrapscrapyItem ()
-    item['Heading'] = str (sel.xpath ('//*[@id="content"]/h3/text()').extract ()) + str (sel.xpath ('//*[@id="content"]/dl/h3/text()').extract ())
-    item['Content'] = str (map (unicode.strip, sel.xpath ('//*[@id="content"]/p/text()').extract ()))
-    item['Content'] += str (map (unicode.strip, sel.xpath ('//dl//text()').extract ()))
-    item['Source_Website'] = "http://scrapy.org"
-    return item
+class ScrapyscrapSpider (BaseSpider):
+    name = "ss"
+    allowed_domains = ["scrapy.org"]
+    start_urls = ['http://scrapy.org/']
 
+    def parse(self, response):
+        sel = Selector(response)
+        item = ScrapscrapyItem()
+        item['Heading'] = str(
+            sel.xpath('/html/body/div[2]/div/div[1]/div/div[1]/h1').extract())
+        item['Content'] = str(
+            sel.xpath('/html/body/div[2]/div/div[1]/div/div[1]/p/text()').extract())
+        item['Source_Website'] = "http://scrapy.org"
+        return item
