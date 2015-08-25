85e6e8b05d36c057ab0cd23b2dd096cdfb1281e5
Tapasweni Pathak <tapaswenipathak@gmail.com>
2015-08-26 05:27:42 +0530

    pep8 ScrapyWithDjango Project

--- ScrapyWithDjango/Try/Try/spiders/TrySpider.py
+++ ScrapyWithDjango/Try/Try/spiders/TrySpider.py
@@ -1,27 +1,30 @@
 from scrapy.selector import Selector
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
-from Try.items import  TryItem
-import re, sys, os
+from Try.items import TryItem
+import re
+import sys
+import os
 
 sys.path.append('path/to/project')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_name.settings")
 
 from product.models import Product
 
-class  TrySpider (CrawlSpider) :
+
+class TrySpider (CrawlSpider):
   handle_httpstatus_list = [302]
-  name = "try"  #Your spider namme
-  allowed_domains = ["try.com"]  #Domain name
-  start_urls = ['www.try.com/abc.html'] #Start URL, can be more than one
-  
+  name = "try"  # Your spider namme
+  allowed_domains = ["try.com"]  # Domain name
+  start_urls = ['www.try.com/abc.html']  # Start URL, can be more than one
+
 						]
-  #Can have multiple rules, exits on first rule matched
+  # Can have multiple rules, exits on first rule matched
   rules = (
       Rule(SgmlLinkExtractor(allow=("", ), unique=False), callback='parse_item',  follow= True),
   )
   
-  #Calls parse_item when rule matched
+  # Calls parse_item when rule matched
   def parse_item(self, response) :
     sel = Selector (response)
     item =  TryItem ()
@@ -29,7 +32,7 @@ class  TrySpider (CrawlSpider) :
     item['Category'] = #xpath for some other field
     item['Product_URL'] = response.request.url #this gives the current url cralwed
     
-    #Django database 
+    # Django database 
 
     item1 = Product() 
     item1.field_name = #assign value
