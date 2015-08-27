6892be927fcad8e2527f32eecee9f5c169b0eed1
Tapasweni Pathak <tapaswenipathak@gmail.com>
2015-08-27 05:46:57 +0530

    pep8 files

--- ScrapyWithSelenium/Try1/Try1/spiders/try1spider.py
+++ ScrapyWithSelenium/Try1/Try1/spiders/try1spider.py
@@ -1,4 +1,4 @@
-#This codes need some cleaning
+# This codes need some cleaning
 
 
 from scrapy.selector import Selector
@@ -17,42 +17,42 @@ from time import sleep
 import time
 import re
 
-class TrySpider (CrawlSpider) :
-  handle_httpstatus_list = [302]
-  name = "try"
-  allowed_domains = ["try.com"] #Add allowed domains, leave blank to allow all
-  start_urls = [''] #Add start urls in this
-  
-  rules = (
-      Rule(SgmlLinkExtractor(allow=(".*", ), unique=True), callback='parse_item', follow= True),
-  )
-  def __init__(self):
-    CrawlSpider.__init__(self)
-    self.verificationErrors = []
-    self.selenium = selenium("localhost", 4444, "*chrome", "http://www.try.com")
-    self.selenium.start()
 
-  def __del__(self):
-    self.selenium.stop()
-    print self.verificationErrors
-    CrawlSpider.__del__(self)
+class TrySpider (CrawlSpider):
+    handle_httpstatus_list = [302]
+    name = "try"
+    # Add allowed domains, leave blank to allow all
+    allowed_domains = ["try.com"]
+    start_urls = ['']  # Add start urls in this
 
-  def parse_item(self, response) :
-    sel = Selector (response)
-    item = TryItem ()
-    sel1 = self.selenium
-    sel1.open(response.url)
-    #Wait for javscript to load in Selenium
-    time.sleep(2.5)
-    #Do some crawling of javascript created content with Selenium
-    item['Source_Website'] = "https://www.try.com"
-    item['Title'] = sel1.get_text ('') #write your xpath, it's a bit different for selenium
-    
-    #return or yield when you are done
-    return item
+    rules = (
+        Rule(SgmlLinkExtractor(allow=(".*", ), unique=True),
+             callback='parse_item', follow=True),
+    )
 
+    def __init__(self):
+        CrawlSpider.__init__(self)
+        self.verificationErrors = []
+        self.selenium = selenium(
+            "localhost", 4444, "*chrome", "http://www.try.com")
+        self.selenium.start()
 
+    def __del__(self):
+        self.selenium.stop()
+        print self.verificationErrors
+        CrawlSpider.__del__(self)
 
+    def parse_item(self, response):
+        sel = Selector(response)
+        item = TryItem()
+        sel1 = self.selenium
+        sel1.open(response.url)
+        # Wait for javscript to load in Selenium
+        time.sleep(2.5)
+        # Do some crawling of javascript created content with Selenium
+        item['Source_Website'] = "https://www.try.com"
+        # write your xpath, it's a bit different for selenium
+        item['Title'] = sel1.get_text('')
 
-
-
+        # return or yield when you are done
+        return item
